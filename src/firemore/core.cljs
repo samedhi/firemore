34b2f15d2301fc94560a4ee609854de040da718b
Stephen Cagle <samedhi@gmail.com>
2019-06-29 15:59:12 -0700

    Write some  more on authentication

--- src/firemore/core.cljs
+++ src/firemore/core.cljs
@@ -1,5 +1,9 @@
 (ns firemore.core)
 
+;; interop
+
+;; database
+
 (defn grab
   "Get the document at `reference` in the Firestore database.
 
@@ -69,8 +73,28 @@
   closed  -> `clojure.core.async/close!`"
   [reference])
 
+;; authentication
+
+(defn get-user
+  "Returns a channel. Will put! user map or :firemore/logged-out as user state changes.
+
+  Returns a channel. puts! :firemore/logged-out when client is not logged into Firestore.
+  Atom will contain a user map when client is logged in to Firestore. User map has the
+  following form:
+
+  {:uid <application_unique_id>
+  :email <user_email_address>
+  :name <user_identifier>
+  :photo <url_to_a_photo_for_this_user>}
+
+  Note: :uid will always be present. :email, :name, :photo may be present depending
+  on sign-in provider and/or whether you have set their values."
+  [])
+
+;; watchers
+
 (defn hydrate
-  "Adds functionality to atom `atm` to allow observation of the Firestore database.
+  "Add functionality to atom `atm` to allow observation of the Firestore database.
 
   Returns nil. Noop if atom is already hydrated. Adds a watch that causes atom to
   automatically sync its :paths and :references root keys. :paths should be a
@@ -83,7 +107,13 @@
   [atm])
 
 (defn unhydrate
-  "Removes functionality on `atm` that may have been added by `firemore/hydrate`.
+  "Removes functionality on `atm` that may have been added by `hydrate`."
+  [atm])
 
-  Returns nil. Noop if atom is already unhydrated."
+(defn watch-user
+  "Add functionality to atom `atm` so that `:user` reflects latest value from `get-user`"
+  [atm])
+
+(defn unwatch-user
+  "Removes functionality on `atm` that may have been added by `watch-user`"
   [atm])
