6be6da4ef613d89e64bb3c2e290d88f171b80808
Stephen Cagle <samedhi@gmail.com>
2019-06-29 11:13:57 -0700

    Include server to allow local development
    > python simple_http_server_cors.py

---
+++ simple_http_server_cors.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# Usage: python cors_http_server.py <port>
+
+try:
+    # try to use Python 3
+    from http.server import HTTPServer, SimpleHTTPRequestHandler, test as test_orig
+    import sys
+    def test (*args):
+        test_orig(*args, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
+except ImportError: # fall back to Python 2
+    from BaseHTTPServer import HTTPServer, test
+    from SimpleHTTPServer import SimpleHTTPRequestHandler
+
+class CORSRequestHandler (SimpleHTTPRequestHandler):
+    def end_headers (self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        SimpleHTTPRequestHandler.end_headers(self)
+
+if __name__ == '__main__':
+    test(CORSRequestHandler, HTTPServer)
