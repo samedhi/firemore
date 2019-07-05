72c66b635ada9a1961580278307f8ebd11d1800b
Stephen Cagle <samedhi@gmail.com>
2019-07-04 17:02:51 -0700

    Get the interactive environment working

--- project.clj
+++ project.clj
@@ -8,17 +8,20 @@
                  [cryogen-core "0.2.1"]
                  [org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.520"]
-                 [org.clojure/test.check                  "0.10.0-alpha3" :scope "test"]
+                 [org.clojure/test.check "0.10.0-alpha3" :scope "test"]
                  [ring/ring-devel "1.7.1"]
-                 [ring-server "0.5.0"]]
+                 [ring-server "0.5.0"]
+                 [figwheel-sidecar "0.5.18"]]
   :plugins [[lein-cljsbuild "1.1.6"]
-            [lein-doo "0.1.10"] 
+            [lein-doo "0.1.10"]
             [lein-figwheel "0.5.13"]
             [lein-ring "0.12.5"]]
+  :source-paths ["src"]
+  :clean-targets ^{:protect false} ["resource/public/js"]
   :cljsbuild {:builds
               [{:id "dev"
                 :figwheel true
-                :source-paths ["src" "test"]
+                :source-paths ["src"]
                 :compiler {:main firemore.core
                            :optimizations :none
                            :output-dir "resources/public/js/out-dev"
@@ -31,5 +34,7 @@
                            :output-dir "resources/public/js/out-test"
                            :output-to "resources/public/js/test.js"}}]}
   :main cryogen.core
+  :profiles {:dev {:dependencies [[cider/piggieback "0.4.1"]]
+                   :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}}}
   :ring {:init cryogen.server/init
          :handler cryogen.server/handler})
