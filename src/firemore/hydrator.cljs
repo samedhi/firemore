5d96423ce4c6f2f436e71eba11fec17b35c5d38c
Stephen Cagle <stephen@read-line.com>
2019-08-22 20:34:41 -0700

    Actually, diff can be a lot simpler

--- src/firemore/hydrator.cljs
+++ src/firemore/hydrator.cljs
@@ -1,9 +1,13 @@
 (ns firemore.hydrator
   (:require
+   [cljs.core.async :as async]
    [clojure.data :as data]
-   [clojure.set :as set]))
+   [clojure.set :as set]
+   [firemore.firestore :as firestore]))
 
-(defn diff [old new]
+(enable-console-print!)
+
+(defn diff-old [old new]
   (let [[only-a only-b _] (data/diff old new)
         changed (reduce
                  (fn [m k]
@@ -14,3 +18,24 @@
      :added   (reduce dissoc (or only-b {}) (keys changed))
      :changed changed}))
 
+(defn diff [old new]
+  (let [[only-a only-b _] (data/diff old new)]
+    {:removed (or only-a {})
+     :added (or only-b {})}))
+
+(defn handle-removed [{:keys [removed] :as m}]
+  (doseq [[k v] removed]
+    (println "Destroying observer for" k "->" v )
+    (-> v meta :unsubscribe (apply []))))
+
+(defn handle-added [added]
+  (reduce-kv
+   (fn [m k v]
+     (println "Creating observer for" k "->" v )
+     (let [{:keys [c unsubscribe] :as m2} (firestore/listen-db v)]
+       (async/go
+         (when-let [document (async/<! )]
+           ))
+       (assoc m k (with-meta v (select-keys m2 [:unsubscribe])))))
+   {}
+   added))
--- test/firemore/hydrator_test.cljs
+++ test/firemore/hydrator_test.cljs
@@ -3,8 +3,8 @@
    [cljs.test :as t :include-macros true]
    [firemore.hydrator :as sut]))
 
-(t/deftest diff-test
-  (t/are [old new expected] (= expected (sut/diff old new))
+(t/deftest diff-old-test
+  (t/are [old new expected] (= expected (sut/diff-old old new))
     {} {:a 1} {:removed {}
                :added   {:a 1}
                :changed {}}
@@ -14,3 +14,14 @@
     {:a 1} {:a 2} {:removed {}
                    :added   {}
                    :changed {:a [1 2]}}))
+
+(t/deftest diff-test
+  (t/are [old new expected] (= expected (sut/diff old new))
+    {} {:a 1} {:removed {}
+               :added   {:a 1}}
+    {:a 1} {} {:removed {:a 1}
+               :added   {}}
+    {:a 1} {:a 2} {:removed {:a 1}
+                   :added   {:a 2}}))
+
+
