254e2a215fdd03c3a55147920d0c6465fc52397c
Stephen Cagle <stephen@read-line.com>
2019-09-01 17:56:38 -0700

    Fix authentication test so that only calls (done) once

--- test/firemore/authentication_test.cljs
+++ test/firemore/authentication_test.cljs
@@ -4,19 +4,21 @@
    [cljs.core.async :as async]
    [cljs.test :as t :include-macros true]
    [clojure.string :as string]
-   [firemore.authentication :as authentication]
-   [firemore.config :as config]))
+   [firemore.authentication :as authentication]))
 
 (t/deftest login-anonymously!-test
   (t/async
    done
    (let [not-blank? (complement string/blank?)
+         done-called (atom false)
+         done-fx #(when-not @done-called
+                    (reset! done-called true)
+                    (done))
          watcher (fn [_ _ _ n]
                    (when n
                      (t/is (-> n :uid not-blank?))
                      (t/is (-> n :anonymous? true?))
                      (t/is (some? n))
-                     (done)))]
-     (t/is (nil? config/user-atom))
+                     (done-fx)))]
      (add-watch authentication/user-atom :watch-test watcher)
      (sut/login-anonymously!))))
