00cdea755d370ebc2ec2e3ec0502be4095c38065
Stephen Cagle <samedhi@gmail.com>
2019-10-09 14:56:26 -0700

    Upgrade version number to celebrate using cljsjs

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.samedhi/firemore "0.1.0"
+(defproject org.clojars.samedhi/firemore "0.2.0"
   :description "A opinionated binding between clojurescript and Firebase"
   :url "https://firemore.org"
   :license {:name "MIT License"
