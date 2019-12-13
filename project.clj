e4e188e2c8c9f2b7a45e6c588ed8b9911b1f8c48
Stephen Cagle <stephen@read-line.com>
2019-12-12 20:50:45 -0800

    Raise version number in celebration of transactions!

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.samedhi/firemore "0.2.6"
+(defproject org.clojars.samedhi/firemore "0.3.0"
   :description "A opinionated binding between clojurescript and Firebase"
   :url "https://firemore.org"
   :license {:name "MIT License"
