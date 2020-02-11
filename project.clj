fb1f32b534b0b2cd0fafbabc94f7968fec46a675
Stephen Cagle <stephen@read-line.com>
2020-02-10 16:26:57 -0800

    Trying to debug why we can't pull firebase.tools

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.samedhi/firemore "0.3.2"
+(defproject org.clojars.samedhi/firemore "0.3.3"
   :description "A opinionated binding between clojurescript and Firebase"
   :url "https://firemore.org"
   :license {:name "MIT License"
