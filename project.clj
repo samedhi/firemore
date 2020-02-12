218d8854a121741ec46a05bf19d00b0f1e4a8e0b
Stephen Cagle <stephen@read-line.com>
2020-02-11 17:38:26 -0800

    Finally (I think)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.samedhi/firemore "0.3.7"
+(defproject org.clojars.samedhi/firemore "0.3.8"
   :description "A opinionated binding between clojurescript and Firebase"
   :url "https://firemore.org"
   :license {:name "MIT License"
