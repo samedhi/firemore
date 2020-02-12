ab9423439d5cdb524fbf0f38740176efd55375c1
Stephen Cagle <stephen@read-line.com>
2020-02-11 17:11:50 -0800

    There has got to be a better way of testing this stuff out

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.samedhi/firemore "0.3.5"
+(defproject org.clojars.samedhi/firemore "0.3.6"
   :description "A opinionated binding between clojurescript and Firebase"
   :url "https://firemore.org"
   :license {:name "MIT License"
