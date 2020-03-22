01c79208b5ac70d0775680a63b9f3635bd1f4263
Matúš Láslofi <matus.laslofi@gmail.com>
2020-03-22 14:25:16 +0100

    No https prefix for storage bucket and auth domain
    This plays well with Firebase Auth UI which expects the Auth Domain to be just a domain, without protocol.

--- src/firemore/config.cljs
+++ src/firemore/config.cljs
@@ -44,9 +44,9 @@
 
 (def FIREBASE_DATABASE_URL (https FIREBASE_PROJECT_ID ".firebaseio.com"))
 
-(def FIREBASE_STORAGE_BUCKET (https FIREBASE_PROJECT_ID ".appspot.com"))
+(def FIREBASE_STORAGE_BUCKET (str FIREBASE_PROJECT_ID ".appspot.com"))
 
-(def FIREBASE_AUTH_DOMAIN (https FIREBASE_PROJECT_ID ".firebaseapp.com"))
+(def FIREBASE_AUTH_DOMAIN (str FIREBASE_PROJECT_ID ".firebaseapp.com"))
 
 ;; Constants
 
