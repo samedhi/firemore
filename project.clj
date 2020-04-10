47f576ca9aa9237a05c1b0bbf31c4c8c8b897a06
Stephen Cagle <stephen@read-line.com>
2020-04-10 10:14:56 -0700

    Bump version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.samedhi/firemore "0.5.2"
+(defproject org.clojars.samedhi/firemore "0.5.3"
   :description "A opinionated binding between clojurescript and Firebase"
   :url "https://firemore.org"
   :license {:name "MIT License"
