5e2a41a6bae428e4e72ee6c5da99d5af3cb4a22f
samedhi <samedhi@gmail.com>
2020-06-10 11:17:26 -0700

    Bump version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.samedhi/firemore "0.6.0"
+(defproject org.clojars.samedhi/firemore "0.6.1"
   :description "A opinionated binding between clojurescript and Firebase"
   :url "https://firemore.org"
   :license {:name "MIT License"
